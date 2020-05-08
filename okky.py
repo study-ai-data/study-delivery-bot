4f3139611d0f80d9a027b684f65d3d456805a62d
kimkyeongnam <kkyy0126@naver.com>
2020-05-08 11:20:41 +0900

    clean up

--- okky.py
+++ okky.py
@@ -21,31 +21,24 @@ def isDateInRange(created_at):
 
 class Okky:
     def __init__(self):
-        self.name = "okky"
+        self.name = "[Okky]"
         self.site = "https://okky.kr"
         self.res = urlopen(self.site + "/articles/gathering?offset=0&max=20&sort=id&order=desc")
         self.soup = BeautifulSoup(self.res, 'html.parser')
 
     def get_content(self):
         issue_content = ""
-        if self.name == "okky":
-            article_list = self.soup.select('#list-article ul > li.list-group-item')
-            for content in article_list:
-                title = content.select('h5 > a')[0]
-                published_at = content.select('div.date-created span.timeago')[0].get_text()
-                item = published_at + " " + str(title).replace('href="', 'href="' + self.site).replace("\n", "").replace('  ', '').strip() + '<br>\n'
-                if '마감' not in str(title) and isDateInRange(published_at):
-                    issue_content += item
-                else:
-                    print('[filtered] ', item)
-        else:
-            print("준비중!")
+        article_list = self.soup.select('#list-article ul > li.list-group-item')
+        for content in article_list:
+            title = content.select('h5 > a')[0]
+            published_at = content.select('div.date-created span.timeago')[0].get_text()
+            item = published_at + " " + str(title).replace('href="', 'href="' + self.site).replace("\n", "").replace('  ', '').strip() + '<br>\n'
+            if '마감' not in str(title) and isDateInRange(published_at):
+                issue_content += item
+            else:
+                print('[filtered] ', item)
         return issue_content
 
     def get_title(self):
-        issue_title = ""
-        if self.name == "okky":
-            issue_title = "[OKKY] 스터디 모집 %s" % (today.strftime("%Y년 %m월 %d일 %H시".encode('unicode-escape').decode()).encode().decode('unicode-escape'))
-        else:
-            print("준비중!")
+        issue_title = self.name + " 스터디 모집 %s" % (today.strftime("%Y년 %m월 %d일 %H시".encode('unicode-escape').decode()).encode().decode('unicode-escape'))
         return issue_title
